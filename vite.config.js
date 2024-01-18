60af4398df18b66346450ffa2aa85f3e66cbebfe
“yi” <zihong2020@foxmail.com>
2024-01-18 19:01:57 +0800

    feat: add git lint

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@
  * @Date: 2024-01-18 11:54:59
  * @LastEditors: hy
  * @Description:
- * @LastEditTime: 2024-01-18 12:21:52
+ * @LastEditTime: 2024-01-18 18:56:51
  * @FilePath: /mini-react/vite.config.js
  * @Copyright 2024 hy, All Rights Reserved.
  **/
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
   test: {
@@ -17,4 +18,9 @@ export default defineConfig({
   resolve: {
     extensions: ["js", "jsx"],
   },
+  plugins: [
+    eslintPlugin({
+      include: ["src/**/*.jsx", "src/**/*.js", "src/*.js", "src/*.jsx"],
+    }),
+  ],
 });
